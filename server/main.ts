7210353d84324922933977db388c29be832372bd
Tim <94912397+30Msearchtime@users.noreply.github.com>
2024-02-13 09:00:39 +0100

    Update main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,48 +1,34 @@
 import * as alt from 'alt-server';
-
-interface PlayerPosition {
-  x: number;
-  y: number;
-  z: number;
-}
-
-const playerPositions: Map<number, PlayerPosition> = new Map();
+import { savePlayerPosition, loadPlayerPosition } from './database'; // Stelle sicher, dass der Pfad zur database.ts Datei korrekt ist.
 
 alt.on('playerConnect', (player) => {
-  playerPositions.set(player.id, null);
-  alt.log(`Player ${player.id} connected.`);
+    alt.log(`Player ${player.id} connected.`);
 
-  player.on('chatMessage', (message) => {
-    if (message === '/savepos') {
-      const position = player.pos;
-      playerPositions.set(player.id, { x: position.x, y: position.y, z: position.z });
-      player.emit('positionSaved');
-    } else if (message === '/loadpos') {
-      const position = playerPositions.get(player.id);
-      if (!position) {
-        player.emit('positionError', 'Keine gespeicherte Position gefunden.');
-        return;
-      }
-
-      player.pos = new alt.Vector3(position.x, position.y, position.z);
-      player.emit('teleportedToPosition');
-    }
-  });
-});
-
-alt.onClient('savePosition', (player) => {
-  const position = player.pos;
-  playerPositions.set(player.id, { x: position.x, y: position.y, z: position.z });
-  player.emit('positionSaved');
-});
-
-alt.onClient('teleportToSavedPosition', (player) => {
-  const position = playerPositions.get(player.id);
-  if (!position) {
-    player.emit('positionError', 'Keine gespeicherte Position gefunden.');
-    return;
-  }
-
-  player.pos = new alt.Vector3(position.x, position.y, position.z);
-  player.emit('teleportedToPosition');
+    player.on('chatMessage', async (message) => {
+        if (message === '/savepos') {
+            const position = player.pos;
+            try {
+                await savePlayerPosition(player.id.toString(), position.x, position.y, position.z);
+                player.emit('positionSaved');
+                alt.log(`Position für Spieler ${player.id} gespeichert.`);
+            } catch (error) {
+                console.error('Fehler beim Speichern der Position:', error);
+                player.emit('positionError', 'Position konnte nicht gespeichert werden.');
+            }
+        } else if (message === '/loadpos') {
+            try {
+                const position = await loadPlayerPosition(player.id.toString());
+                if (position) {
+                    player.pos = new alt.Vector3(position.posX, position.posY, position.posZ);
+                    player.emit('teleportedToPosition');
+                    alt.log(`Spieler ${player.id} zu gespeicherter Position teleportiert.`);
+                } else {
+                    player.emit('positionError', 'Keine gespeicherte Position gefunden.');
+                }
+            } catch (error) {
+                console.error('Fehler beim Laden der Position:', error);
+                player.emit('positionError', 'Position konnte nicht geladen werden.');
+            }
+        }
+    });
 });
