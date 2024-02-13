63a18fad4fc36f52382d725c218d1667a09db658
Tim <94912397+30Msearchtime@users.noreply.github.com>
2024-02-13 08:57:32 +0100

    Update main.ts

--- client/main.ts
+++ client/main.ts
@@ -1,13 +1,13 @@
 import * as alt from 'alt-client';
 
 alt.onServer('positionSaved', () => {
-  alt.log('Deine Position wurde gespeichert.');
+    alt.log('Deine Position wurde gespeichert.');
 });
 
 alt.onServer('teleportedToPosition', () => {
-  alt.log('Du wurdest zu deiner gespeicherten Position teleportiert.');
+    alt.log('Du wurdest zu deiner gespeicherten Position teleportiert.');
 });
 
 alt.onServer('positionError', (message: string) => {
-  alt.log(message);
+    alt.log(message);
 });
